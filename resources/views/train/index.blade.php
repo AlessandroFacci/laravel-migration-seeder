1fca4891904d8fe61fe594125daa4737f3b0f3ac
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-16 17:47:31 +0200

    stamp trains

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -3,12 +3,34 @@
 @section('main-content')
 <section class="container mt-5">
   @forelse ($trains as $train)
-
-  {{dd($trains)}}
   
       <ul>
        <li>
          <strong>Azienda:</strong> {{$train->azienda}}
+       </li>
+       <li>
+        <strong>Stazione di partenza:</strong> {{$train->stazione_di_partenza}}
+       </li>
+       <li>
+        <strong>Stazione di arrivo:</strong> {{$train->stazione_di_arrivo}}
+       </li>
+       <li>
+        <strong>Orario di partenza:</strong> {{$train->orario_di_partenza}}
+       </li>
+       <li>
+        <strong>Orario di arrivo:</strong> {{$train->orario_di_arrivo}}
+       </li>
+       <li>
+        <strong>Codice treno:</strong> {{$train->codice_treno}}
+       </li>
+       <li>
+        <strong>Numero carrozze:</strong> {{$train->numero_carrozze}}
+       </li>
+       <li>
+        <strong>In orario:</strong> {{$train->in_orario}}
+       </li>
+       <li>
+        <strong>Cancellato:</strong> {{$train->cancellato}}
        </li>
       </ul>
   @empty
