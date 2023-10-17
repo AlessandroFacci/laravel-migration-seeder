3ce921ba35fc442115797c28bd5f0def4acb9b34
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-17 21:51:15 +0200

    add train cards

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -2,41 +2,51 @@
 
 @section('main-content')
 <section class="container mt-5">
-  @forelse ($trains as $train)
+  <div class="row g-3">
+    @forelse ($trains as $train)
+    <div class="col-4">
+      <div class="card">
+        <div class="card-header">
+          <strong>{{$train->azienda}}</strong> 
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>
+             <strong>Stazione di partenza:</strong> {{$train->stazione_di_partenza}}
+            </li>
+            <li>
+             <strong>Stazione di arrivo:</strong> {{$train->stazione_di_arrivo}}
+            </li>
+            <li>
+             <strong>Orario di partenza:</strong> {{$train->orario_di_partenza}}
+            </li>
+            <li>
+             <strong>Orario di arrivo:</strong> {{$train->orario_di_arrivo}}
+            </li>
+            <li>
+             <strong>Codice treno:</strong> {{$train->codice_treno}}
+            </li>
+            <li>
+             <strong>Numero carrozze:</strong> {{$train->numero_carrozze}}
+            </li>
+            <li>
+             <strong>In orario:</strong> {{$train->in_orario ? 'Sì' : 'No'}}
+            </li>
+            <li>
+             <strong>Cancellato:</strong> {{$train->cancellato ? 'Sì' : 'No'}}
+            </li>
+           </ul>
+        </div>
+      </div>
+    </div>
+    @empty
+    <h3>Non ci sono risultati</h3>
+     @endforelse
+  </div>
+ 
   
-      <ul>
-       <li>
-         <strong>Azienda:</strong> {{$train->azienda}}
-       </li>
-       <li>
-        <strong>Stazione di partenza:</strong> {{$train->stazione_di_partenza}}
-       </li>
-       <li>
-        <strong>Stazione di arrivo:</strong> {{$train->stazione_di_arrivo}}
-       </li>
-       <li>
-        <strong>Orario di partenza:</strong> {{$train->orario_di_partenza}}
-       </li>
-       <li>
-        <strong>Orario di arrivo:</strong> {{$train->orario_di_arrivo}}
-       </li>
-       <li>
-        <strong>Codice treno:</strong> {{$train->codice_treno}}
-       </li>
-       <li>
-        <strong>Numero carrozze:</strong> {{$train->numero_carrozze}}
-       </li>
-       <li>
-        <strong>In orario:</strong> {{$train->in_orario}}
-       </li>
-       <li>
-        <strong>Cancellato:</strong> {{$train->cancellato}}
-       </li>
-      </ul>
-  @empty
-  <h3>Non ci sono risultati</h3>
       
-  @endforelse
+  
  </section>
 @endsection
 
